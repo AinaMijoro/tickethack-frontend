ee166ca746608bac565ea79a61918be54fe963e1
Aina Mijoro <aina.mijoro@gmail.com>
2023-01-24 22:27:31 +0100

    test

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 document.querySelector("#searchButton").addEventListener("click", function () {
-    //   récupérer le nom de la ville du champ de saisie
-    let myTrips = [document.querySelector("#departure").value,
-                   document.querySelector ('#arrival').value,
-                   document.querySelector("#date").value]
-    // faire la requéte a la route voulu via le fecth
-    fetch("http://localhost:3000/trips", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ myTrips }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.result) {
-          document.querySelector("#cityList").innerHTML += `
-      <div class="cityContainer">
-          <p class="name">${data.weather.cityName}</p>
-          <p class="description">${data.weather.description}</p>
-          <img class="weatherIcon" src="images/${data.weather.main}.png"/>
-          <div class="temperature">
-              <p class="tempMin">${data.weather.tempMin}°C</p>
-              <span>-</span>
-              <p class="tempMax">${data.weather.tempMax}°C</p>
-          </div>
-          <button class="deleteCity" id="${data.weather.cityName}">Delete</button>
-      </div> `;
-        }
-        deleteCity();
-      });
-  });
+  const myTrip = [document.querySelector("#departureInput").value,
+  document.querySelector("#arrivalInput").value,
+  document.querySelector("#dateInput").value,] ;
+  fetch("http://localhost:3000/trips", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ myTrip }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+        if(data)
+        {for (let i=0; i<data.length; i++)
+            document.querySelector("#tripResult").innerHTML += `
+              <span id="depart">${data[i].departure}</span>
+              <span id="arrivee">${data[i].arrival}</span>
+              <span id="heure">${data[i].date.getHours()}+':'${data[i].date.getMinutes()}</span>
+              <span id="price">${data[i].price}+'€'}</span>
+              <button id="Book">Book</button>
+            })`;}
+            else {
+                document.querySelector("#tripResult").innerHTML = `
+                <img src="images/notfound.png"/>
+                <span id ="error">${data.error}</span>`
+                
+                
+            }
+      
+    });
+});
