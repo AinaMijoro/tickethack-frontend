cb8311410d06dcd5fbb618b978c44518f8ed7163
Yoann Andrieux <yoann.andrieux@gmail.com>
2023-01-25 12:29:19 +0100

    test

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
+/* Select trip by departure, arrival and date */ 
 document.querySelector("#searchButton").addEventListener("click", function () {
-  const myTrip = {departure: document.querySelector("#departureInput").value,
-  arrival: document.querySelector("#arrivalInput").value,
-  date: document.querySelector("#dateInput").value,};
-  fetch("http://localhost:3000/trips", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ myTrip }),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-        if(data)
-        {for (let i=0; i<data.length; i++)
-            document.querySelector("#tripResult").innerHTML += `
-              <span id="depart">${data[i].departure}</span>
-              <span id="arrivee">${data[i].arrival}</span>
-              <span id="heure">${data[i].date.getHours()}+':'${data[i].date.getMinutes()}</span>
-              <span id="price">${data[i].price}+'€'}</span>
-              <button id="Book">Book</button>
-            })`;}
-            else {
-                document.querySelector("#tripResult").innerHTML = `
-                <img src="images/notfound.png"/>
-                <span id ="error">${data.error}</span>`
-                
-                
+    const myTrip = {
+        departure: document.querySelector("#departureInput").value,
+        arrival: document.querySelector("#arrivalInput").value,
+        date: document.querySelector("#dateInput").value,
+    };
+
+    fetch("http://localhost:3000/trips", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(myTrip),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            for (let i = 0; i < data.Data.length; i++) {
+                const trip = data.Data[i];
+                //console.log(trip.departure)
+
+                const newRow = document.createElement("div")
+
+                document.querySelector("#right-card").innerHTML += `
+                <div class="tripRow">
+                  <div class="trip-info">
+                    <h3>
+                    <span id="depart">${trip.departure}</span>
+                    <span id="arrivee">${trip.arrival}</span>
+                    <span id="heure">${trip.date}: ${trip.date}</span>
+                    <span id="price">${trip.price}€</span>
+                    </h3>
+                  </div>
+                <button id="Book">Book</button>
+                </div>`;
             }
-      
-    });
+        });
 });
